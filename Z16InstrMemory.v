d7bb3d18214028aecd03609d99aab1e3b3238c7d
jinnosukeKato <jinnosukeKato@gmail.com>
2025-10-08 22:56:34 +0900

    LOAD命令のデータパス完成

--- Z16InstrMemory.v
+++ Z16InstrMemory.v
@@ -7,10 +7,9 @@ module Z16InstrMemory(
   wire [15:0] mem[4:0];
   assign o_instr = mem[i_addr[15:1]]; // 上位15bitだけを使う→2byteにアライメントして取り出し
 
-  // 1から10の総和を求めるプログラムを初期値として格納
-  assign mem[0] = 16'h0A19;
-  assign mem[1] = 16'h1220;
-  assign mem[2] = 16'hFF19;
-  assign mem[3] = 16'hFC4F;
-  assign mem[4] = 16'h00FD;
+  assign mem[0] = 16'h406A; // LOAD 4 ZR G2
+  assign mem[1] = 16'h0000;
+  assign mem[2] = 16'h0000;
+  assign mem[3] = 16'h008A; // LOAD 0 ZR G4
+  assign mem[4] = 16'h0000;
 endmodule
