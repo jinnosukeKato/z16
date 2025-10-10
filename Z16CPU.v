5aeef66ac470b12b21e8b65d3079fc684910d06e
jinnosukeKato <jinnosukeKato@gmail.com>
2025-10-10 13:51:20 +0900

    STORE命令を実装

--- Z16CPU.v
+++ Z16CPU.v
@@ -8,12 +8,14 @@ module Z16CPU(
   wire [15:0] w_instr; // フェッチした命令
   wire [3:0] w_rd_addr; // RDのアドレス
   wire [3:0] w_rs1_addr; // RS1のアドレス
+  wire [3:0] w_rs2_addr;
   wire [15:0] w_imm; // 即値
   wire w_rd_wen; // レジスタ書き込み有効化信号
   wire w_mem_wen; // メモリ書き込み有効化信号
   wire [3:0] w_alu_ctrl; // ALU演算制御信号線
 
   wire [15:0] w_rs1_data; // RS1のデータ
+  wire [15:0] w_rs2_data;
 
   wire [15:0] w_alu_data; // ALUの演算結果
   wire [15:0] w_mem_rdata; // メモリからの読み出しデータ
@@ -35,6 +37,7 @@ module Z16CPU(
     .i_instr(w_instr),
     .o_rd_addr(w_rd_addr),
     .o_rs1_addr(w_rs1_addr),
+    .o_rs2_addr(w_rs2_addr),
     .o_imm(w_imm),
     .o_rd_wen(w_rd_wen),
     .o_mem_wen(w_mem_wen),
@@ -45,8 +48,8 @@ module Z16CPU(
     .i_clk(i_clk),
     .i_rs1_addr(w_rs1_addr),
     .o_rs1_data(w_rs1_data),
-    .i_rs2_addr(),
-    .o_rs2_data(),
+    .i_rs2_addr(w_rs2_addr),
+    .o_rs2_data(w_rs2_data),
     .i_rd_addr(w_rd_addr),
     .i_rd_data(w_mem_rdata),
     .i_rd_wen(w_rd_wen)
@@ -63,7 +66,7 @@ module Z16CPU(
     .i_clk(i_clk),
     .i_addr(w_alu_data),
     .i_wen(w_mem_wen),
-    .i_data(),
+    .i_data(w_rs2_data),
     .o_data(w_mem_rdata)
   );
 endmodule
