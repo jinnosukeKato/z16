414c8d9564b5926c9d8fd3378b475669f6820ef0
jinnosukeKato <jinnosukeKato@gmail.com>
2025-10-17 12:20:39 +0900

    分岐命令を実装

--- Z16InstrMemory.v
+++ Z16InstrMemory.v
@@ -7,9 +7,11 @@ module Z16InstrMemory(
   wire [15:0] mem[10:0];
   assign o_instr = mem[i_addr[15:1]]; // 上位15bitだけを使う→2byteにアライメントして取り出し
 
-  assign mem[0] = 16'h0040;
-  assign mem[1] = 16'h605D;
-  assign mem[2] = 16'h0000;
-  assign mem[3] = 16'h0000;
-  assign mem[4] = 16'h006C;
+  assign mem[0] = 16'h0010; // ADD ZR ZR B1
+  assign mem[1] = 16'h0020; // ADD ZR ZR B2
+  assign mem[2] = 16'h0A19; // ADDI 10 B1
+  assign mem[3] = 16'h1220; // ADD B1 B2 B2
+  assign mem[4] = 16'hFF19; // ADDI -1 B1
+  assign mem[5] = 16'hFC4F; // BLT B1 ZR -4
+  assign mem[6] = 16'h00FD; // JRL 0 ZR G11
 endmodule
